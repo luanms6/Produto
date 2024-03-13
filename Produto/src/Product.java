430a7d51f4a66b0eb082ec5cb5da61dca4efead3
Luan Peixoto <142679674+luanms6@users.noreply.github.com>
2024-03-13 11:48:55 -0300

    Add files via upload

---
+++ Produto/src/Product.java
@@ -0,0 +1,60 @@
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    //Sobrecarga - Disponibilizar mais uma versão da mesma operação. a diferença entre elas será a lista de parâmetros.
+    public Product(String name, double pric){
+        this.name = name;
+        this.price = price;
+        this.quantity = 0;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
+}
